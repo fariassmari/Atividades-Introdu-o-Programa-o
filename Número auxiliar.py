d605b2edb622642835d28d5a152c8024de163d87
Mariana Farias <marianafariasbrito@gmail.com>
2025-04-16 10:55:30 -0300

    Update Número auxiliar.py

--- Número auxiliar.py
+++ Número auxiliar.py
@@ -12,3 +12,10 @@ print(numero1)
 
 numero2 = auxiliar
 print(numero2)
+
+"""
+Primeiramente é definido dois números(numero1 e numero2), com o objetivo geral de trocar
+os valores dessas duas variáveis, utilizando uma terceira variável que foi denominada
+de auxiliar, que está sendo usada temporariamente para armazenar um dos valores dos
+dois números definidos durante a troca.
+"""
