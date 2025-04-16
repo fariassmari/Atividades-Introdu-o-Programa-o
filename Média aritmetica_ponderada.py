634eacc9e1a1d4e0c14ce949939e7dd2fb61c1d3
Mariana Farias <marianafariasbrito@gmail.com>
2025-04-16 10:52:49 -0300

    Update and rename ativ 2 questao 7_0804.py to Média aritmetica_ponderada.py

--- Média aritmetica_ponderada.py
+++ Média aritmetica_ponderada.py
@@ -5,7 +5,7 @@ nota3 = int(input('Nota 3: ')) # Estava faltando transformar em inteiro - C
 media_aritmetica = (nota1 + nota2 + nota3) / 3 # Faltando parênteses - E
 media_ponderada = (nota1 * 2 + nota2 * 2 + nota3 * 5) / 10 # Soma dos pesos erradas - E
 
-media_final = media_aritmetica / media_ponderada
+media_final = media_aritmetica + media_ponderada / 2
 
 print('Notas: ', nota1, nota2, nota3) # Estava faltando a vírgula - C
 print('Média aritmética: ', media_aritmetica)
