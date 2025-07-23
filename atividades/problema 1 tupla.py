38cadadf6703afde03df757368ebb07c8edafaa6
Mariana Farias <marianafariasbrito@gmail.com>
2025-07-23 14:39:20 +0000

    corrigir loop de entrada e otimizar cálculo do produto mais caro

--- atividades/problema 1 tupla.py
+++ atividades/problema 1 tupla.py
@@ -7,26 +7,25 @@ Ao final, exibir:
 
 produtos = []
 
-for i in range(20):
+for i in range(2):
     codigo = int(input(f'Digite o código do produto {i + 1}: '))
-    descricao = int(input(f'Digite a descricao do produto {i + 1}: '))
+    descricao = input(f'Digite a descricao do produto {i + 1}: ')
     preco = float(input(f'Digite o preço do produto {i + 1}: '))
 
     produtos.append((codigo, descricao, preco))
 
 # Encontrar o produto mais caro
-precos = []
+
+preco_maximo = max(produto[2] for produto in produtos)
+
 for produto in produtos:
-    preco = produto[2]
-    precos.append(preco)
+    if produto[2] == preco_maximo:
+        descricao = produto[1]
+        print(f'Produto mais caro: {descricao}')
 
-preco_maximo = max(precos)
+media = sum(produto[2] for produto in produtos) / len(produtos)
 
-for produto in preco_maximo:
-    descricao = produto[1]
-    print(f'Produto mais caro: {descricao}')
-
-media = sum(produtos) / len(produtos)
-
-
-
+for produto in produtos:
+    if produto[2] < media:
+        codigo = produto[0]
+        print(f'Código do produto abaixo da média: {codigo}')
